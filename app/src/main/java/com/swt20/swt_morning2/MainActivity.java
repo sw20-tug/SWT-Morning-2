0bfbf5033ebdf7128596066f63fc59c9e93fe3ed
Vinzenz Brodschneider <61780244+brovic09@users.noreply.github.com>
2020-03-25 09:36:45 +0100

    GAMES-013 added localization [GB,VB]

--- app/src/main/java/com/swt20/swt_morning2/MainActivity.java
+++ app/src/main/java/com/swt20/swt_morning2/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, R.string.snackbar_text, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
