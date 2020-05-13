56031be0dc805cf93879ffd1a1afae40835622ed
Marcel Schanes <schanes@student.tugraz.at>
2020-05-13 09:54:54 +0200

    GAMES_010 added custom word functionality[SM, SS]

--- app/src/main/java/com/swt20/swt_morning2/HangmanMenuFragment.java
+++ app/src/main/java/com/swt20/swt_morning2/HangmanMenuFragment.java
@@ -1,15 +1,27 @@
 package com.swt20.swt_morning2;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.gson.Gson;
+
+import java.util.Locale;
+
 public class HangmanMenuFragment extends Fragment {
+
+    private SharedPreferences sharedPreferences;
+    private HangmanGameFragment.WordList wordList;
+
+    private EditText field;
 
     @Override
     public View onCreateView(
@@ -17,6 +29,17 @@ public class HangmanMenuFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
+        String lang = Locale.getDefault().getDisplayLanguage();
+        if(lang.contains("de"))
+        {
+            sharedPreferences = requireContext().getSharedPreferences("HANGMAN_WORDS_DE", 0);
+        } else {
+            sharedPreferences = requireContext().getSharedPreferences("HANGMAN_WORDS_EN", 0);
+        }
+        String wordsJson = sharedPreferences.getString("WORDS",getString(R.string.hangman_default_words));
+        Gson gson = new Gson();
+        wordList = gson.fromJson(wordsJson, HangmanGameFragment.WordList.class);
+
         return inflater.inflate(R.layout.hangman_menu, container, false);
     }
 
@@ -28,6 +51,21 @@ public class HangmanMenuFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(HangmanMenuFragment.this)
                         .navigate(R.id.action_Menu_to_Game);
+            }
+        });
+
+        field = view.findViewById(R.id.hangmanAddWordTextInput);
+
+        view.findViewById(R.id.hangmanAddWordButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (wordList.addWord(field.getText().toString())){
+                    Toast.makeText(getContext(), getString(R.string.hangman_toast_add_successful), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getContext(), getString(R.string.hangman_toast_add_failed), Toast.LENGTH_LONG).show();
+                }
+                field.setText("");
+
             }
         });
 
