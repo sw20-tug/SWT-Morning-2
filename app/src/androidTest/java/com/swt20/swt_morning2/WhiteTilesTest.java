28c270f4bb0aae71cda859967259c9acee63741b
Vinzenz Brodschneider <61780244+brovic09@users.noreply.github.com>
2020-05-13 10:27:19 +0200

    GAMES_011 added tiles game logic and adapted ui test, set the java version to 1.8, changed the score tracker to reload on get, in order to have the latest information [BV,KP]

--- app/src/androidTest/java/com/swt20/swt_morning2/WhiteTilesTest.java
+++ app/src/androidTest/java/com/swt20/swt_morning2/WhiteTilesTest.java
@@ -1,10 +1,16 @@
 package com.swt20.swt_morning2;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,7 +21,13 @@ import java.util.List;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class WhiteTilesTest {
@@ -24,39 +36,50 @@ public class WhiteTilesTest {
     public ActivityTestRule<MainActivity> activityRule
             = new ActivityTestRule<>(MainActivity.class);
 
-    private List<ViewInteraction> buttons;
+    private List<ViewInteraction> buttons = new ArrayList<>();
 
     @Before
     public void setupButtons(){
-        List<ViewInteraction> buttons = new ArrayList<>();
-        buttons.add(onView(withId(R.id.tilesFieldButton0)));
-        buttons.add(onView(withId(R.id.tilesFieldButton1)));
-        buttons.add(onView(withId(R.id.tilesFieldButton2)));
-        buttons.add(onView(withId(R.id.tilesFieldButton3)));
-        buttons.add(onView(withId(R.id.tilesFieldButton4)));
-        buttons.add(onView(withId(R.id.tilesFieldButton5)));
-        buttons.add(onView(withId(R.id.tilesFieldButton6)));
-        buttons.add(onView(withId(R.id.tilesFieldButton7)));
-        buttons.add(onView(withId(R.id.tilesFieldButton8)));
-        buttons.add(onView(withId(R.id.tilesFieldButton9)));
-        buttons.add(onView(withId(R.id.tilesFieldButton10)));
-        buttons.add(onView(withId(R.id.tilesFieldButton11)));
-        buttons.add(onView(withId(R.id.tilesFieldButton12)));
-        buttons.add(onView(withId(R.id.tilesFieldButton13)));
-        buttons.add(onView(withId(R.id.tilesFieldButton14)));
-        buttons.add(onView(withId(R.id.tilesFieldButton15)));
-        this.buttons = buttons;
+        buttons.clear();
+        WhiteTilesFragment.tileButtonIds.forEach(integer -> {
+            buttons.add(onView(withId(integer)));
+        });
     }
 
 
     @Test
-    public void visitGameAndTouchRandomButtons(){
+    public void clickOnWhiteButton(){
         onView(withId(R.id.whiteTilesButton)).perform(click());
 
         onView(withId(R.id.tiles_menu_button)).perform(click());
-        for (ViewInteraction button : buttons) {
-            button.perform(click());
-        }
+        View whiteButton = getButtonWithColor(Color.WHITE);
+        onView(withId(whiteButton.getId())).perform(click());
+        onView(withText(R.string.you_lose)).inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
 
+    @Test
+    public void clickOnBlackButton(){
+        onView(withId(R.id.whiteTilesButton)).perform(click());
+
+        onView(withId(R.id.tiles_menu_button)).perform(click());
+        View whiteButton = getButtonWithColor(Color.BLACK);
+        ScoreTracker st = new ScoreTracker(activityRule.getActivity().getApplicationContext());
+        int oldScore = st.getScore(Game.TILES);
+        onView(withId(whiteButton.getId())).perform(click());
+        Assert.assertEquals(oldScore + 1, st.getScore(Game.TILES));
+    }
+
+    public View getButtonWithColor(Integer color) {
+        View button = null;
+        for (Integer integer : WhiteTilesFragment.tileButtonIds) {
+            View possibleButton = activityRule.getActivity().findViewById(integer);
+            Drawable background = possibleButton.getBackground();
+            ColorDrawable backgroundDrawable = (ColorDrawable) background;
+            if(backgroundDrawable.getColor() == color) {
+                button = possibleButton;
+                break;
+            }
+        }
+        return button;
     }
 }
--- app/src/main/java/com/swt20/swt_morning2/WhiteTilesFragment.java
+++ app/src/main/java/com/swt20/swt_morning2/WhiteTilesFragment.java
@@ -1,16 +1,26 @@
 package com.swt20.swt_morning2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 
 public class WhiteTilesFragment extends Fragment {
 
+    private HashMap<Integer, Integer> tilesButtons = new HashMap<>();
+    public static List<Integer> tileButtonIds = new ArrayList<>();
 
     @Override
     public View onCreateView(
@@ -24,6 +34,74 @@ public class WhiteTilesFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        tileButtonIds.add(R.id.tilesFieldButton0);
+        tileButtonIds.add(R.id.tilesFieldButton1);
+        tileButtonIds.add(R.id.tilesFieldButton2);
+        tileButtonIds.add(R.id.tilesFieldButton3);
+        tileButtonIds.add(R.id.tilesFieldButton4);
+        tileButtonIds.add(R.id.tilesFieldButton5);
+        tileButtonIds.add(R.id.tilesFieldButton6);
+        tileButtonIds.add(R.id.tilesFieldButton7);
+        tileButtonIds.add(R.id.tilesFieldButton8);
+        tileButtonIds.add(R.id.tilesFieldButton9);
+        tileButtonIds.add(R.id.tilesFieldButton10);
+        tileButtonIds.add(R.id.tilesFieldButton11);
+        tileButtonIds.add(R.id.tilesFieldButton12);
+        tileButtonIds.add(R.id.tilesFieldButton13);
+        tileButtonIds.add(R.id.tilesFieldButton14);
+        tileButtonIds.add(R.id.tilesFieldButton15);
+
+        scrambleButtons(view);
+        tilesButtons.keySet().forEach((buttonId) -> {
+            setupClickListener(view, buttonId);
+        });
+
+    }
+
+    private void setupClickListener(View view, Integer buttonId) {
+        view.findViewById(buttonId).setOnClickListener(view1 -> {
+            Integer integer = tilesButtons.get(buttonId);
+            view.findViewById(buttonId);
+            if (integer != null && integer.equals(Color.WHITE)) {
+                whiteButtonClicked();
+            } else {
+                blackButtonClicked(view);
+            }
+        });
+    }
+
+    private void whiteButtonClicked() {
+        String text = getResources().getString(R.string.you_lose);;
+        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+        NavHostFragment.findNavController(WhiteTilesFragment.this)
+                .navigate(R.id.action_TilesGameFragment_to_TilesMenuFragment);
+    }
+
+    private void blackButtonClicked(View view) {
+        ScoreTracker tracker = new ScoreTracker(view.getContext());
+        tracker.addScore(Game.TILES, 1);
+        scrambleButtons(view);
+    }
+
+    private void scrambleButtons(View view) {
+        Random r = new Random();
+        tileButtonIds.forEach(integer -> {
+            Integer color = r.nextInt(2) == 1 ? Color.WHITE : Color.BLACK;
+            tilesButtons.put(integer, color);
+        });
+        boolean validField = tilesButtons.containsValue(Color.BLACK);
+        if (!validField) {
+            List<Integer> buttonsAsList = new ArrayList<>(tilesButtons.keySet());
+            Integer randomBlackButtonId = buttonsAsList.get(r.nextInt(buttonsAsList.size()));
+            tilesButtons.put(randomBlackButtonId, Color.BLACK);
+        }
+        colorButtons(view);
+    }
+
+    private void colorButtons(View view) {
+        tilesButtons.forEach((buttonId, color) -> {
+            view.findViewById(buttonId).setBackgroundColor(color);
+        });
     }
 
 
