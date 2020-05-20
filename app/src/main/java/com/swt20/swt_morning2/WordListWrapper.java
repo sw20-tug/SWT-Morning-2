3e5856d92b44b644ffdf11fcacb132bffa12ab73
Marcel Schanes <schanes@student.tugraz.at>
2020-05-20 09:22:55 +0200

    GAMES_010 made the wordlist for hangman persistent [SM, SS]

--- app/src/main/java/com/swt20/swt_morning2/WordListWrapper.java
+++ app/src/main/java/com/swt20/swt_morning2/WordListWrapper.java
@@ -12,21 +12,43 @@ import java.util.Locale;
 import java.util.Random;
 
 public class WordListWrapper {
-    private SharedPreferences sharedPreferences;
+    public static SharedPreferences sharedPreferences;
 
     public WordList wordList;
 
     public WordListWrapper(Fragment fragment) {
+
+        String wordsJson = "";
+        Gson gson = new Gson();
         String lang = Locale.getDefault().getDisplayLanguage();
+
         if(lang.contains("de"))
         {
             sharedPreferences = fragment.requireContext().getSharedPreferences("HANGMAN_WORDS_DE", 0);
+            //sharedPreferences.edit().remove("HANGMAN_WORDS_DE").commit();
+            wordsJson = sharedPreferences.getString("HANGMAN_WORDS_DE", fragment.getString(R.string.hangman_default_words));
+
+            this.wordList = gson.fromJson(wordsJson, WordList.class);
+
+            // if we get standard values back store the initial list to the shared prefs
+            if (sharedPreferences.getString("HANGMAN_WORDS_DE", "test").equals("test")) {
+                sharedPreferences.edit().putString("HANGMAN_WORDS_DE", gson.toJson(wordList).toString()).apply();
+            }
         } else {
             sharedPreferences = fragment.requireContext().getSharedPreferences("HANGMAN_WORDS_EN", 0);
+            //sharedPreferences.edit().remove("HANGMAN_WORDS_EN").commit();
+            wordsJson = sharedPreferences.getString("HANGMAN_WORDS_EN", fragment.getString(R.string.hangman_default_words));
+
+            this.wordList = gson.fromJson(wordsJson, WordList.class);
+
+            // if we get standard values back store the initial list to the shared prefs
+            if (sharedPreferences.getString("HANGMAN_WORDS_EN", "test").equals("test")) {
+                sharedPreferences.edit().putString("HANGMAN_WORDS_EN", gson.toJson(wordList).toString()).apply();
+            }
         }
-        String wordsJson = sharedPreferences.getString("WORDS", fragment.getString(R.string.hangman_default_words));
-        Gson gson = new Gson();
-        this.wordList = gson.fromJson(wordsJson, WordList.class);
+
+        //this.wordList.sharedPreferences = sharedPreferences;
+
     }
 
     public class WordList {
@@ -46,16 +68,34 @@ public class WordListWrapper {
         }
 
         public boolean addWord(String word) {
-            if (standardWords.contains(word) || customWords.contains(word))
+            String lang = Locale.getDefault().getDisplayLanguage();
+            if (standardWords.contains(word) || customWords.contains(word) || word.length() < 2)
                 return false;
             else {
                 customWords.add(word);
+                Gson gson = new Gson();
+                if (lang.contains("de")) {
+                    WordListWrapper.sharedPreferences.edit().putString("HANGMAN_WORDS_DE", gson.toJson(this).toString()).apply();
+                } else {
+                    WordListWrapper.sharedPreferences.edit().putString("HANGMAN_WORDS_EN", gson.toJson(this).toString()).apply();
+                }
                 return true;
             }
         }
 
         public boolean removeWord(String word) {
-            return customWords.remove(word);
+            String lang = Locale.getDefault().getDisplayLanguage();
+            Gson gson = new Gson();
+
+            boolean success = customWords.remove(word);
+            if (success) {
+                if (lang.contains("de")) {
+                    WordListWrapper.sharedPreferences.edit().putString("HANGMAN_WORDS_DE", gson.toJson(this).toString()).apply();
+                } else {
+                    WordListWrapper.sharedPreferences.edit().putString("HANGMAN_WORDS_EN", gson.toJson(this).toString()).apply();
+                }
+            }
+            return success;
         }
 
         public boolean isStandardWord(String word) {
