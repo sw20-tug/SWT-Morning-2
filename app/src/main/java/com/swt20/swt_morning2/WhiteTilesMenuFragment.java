e2c74ac73c5f95836ca36236752bd0614cc292fa
Thorsten Mattausch <tmattausch@student.tugraz.at>
2020-05-27 11:29:54 +0200

    GAMES_012 safe timer settings in shared preferences [KP, MT]

--- app/src/main/java/com/swt20/swt_morning2/WhiteTilesMenuFragment.java
+++ app/src/main/java/com/swt20/swt_morning2/WhiteTilesMenuFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,13 +26,41 @@ public class WhiteTilesMenuFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        WhiteTilesSettings settings =
+                new WhiteTilesSettings(getContext().getApplicationContext());
+
+        boolean timerEnabled = settings.getTimerEnabled();
+        Integer timeSetting = settings.getTimerSetting();
+
+        getTimerSwitch(view).setChecked(timerEnabled);
+        getTimerSetting(view).setText(String.format(timeSetting.toString()));
+
+
         view.findViewById(R.id.tiles_menu_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View buttonView) {
+                boolean timerEnabled = getTimerSwitch(view).isChecked();
+                int timeSetting = Integer.parseInt(getTimerSetting(view).getText().toString());
+
+                WhiteTilesSettings settings =
+                        new WhiteTilesSettings(getContext().getApplicationContext());
+
+                settings.setTimerEnabled(timerEnabled);
+                settings.setTimerSetting(timeSetting);
+
                 NavHostFragment.findNavController(WhiteTilesMenuFragment.this)
                         .navigate(R.id.action_Menu_to_Game);
             }
         });
+    }
+
+    private Switch getTimerSwitch(View view) {
+        return (Switch) view.findViewById(R.id.whiteTilesTimerSwitch);
+    }
+
+    private EditText getTimerSetting(View view) {
+        return (EditText) view.findViewById(R.id.whiteTilesTimeSetting);
+
     }
 
 
