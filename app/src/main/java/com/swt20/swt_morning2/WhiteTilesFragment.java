ac50374e51cf8327577b91c88540258a632df485
Thorsten Mattausch <tmattausch@student.tugraz.at>
2020-05-27 11:57:02 +0200

    GAMES_012 implement remaining time [KP, MT]

--- app/src/main/java/com/swt20/swt_morning2/WhiteTilesFragment.java
+++ app/src/main/java/com/swt20/swt_morning2/WhiteTilesFragment.java
@@ -1,10 +1,13 @@
 package com.swt20.swt_morning2;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,9 @@ import androidx.navigation.fragment.NavHostFragment;
 public class WhiteTilesFragment extends Fragment {
 
     private WhiteTilesGameLogic logic;
+    private boolean timerEnabled;
+    private int timerSetting;
+    CountDownTimer timer;
 
     @Override
     public View onCreateView(
@@ -28,6 +34,25 @@ public class WhiteTilesFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
         logic = new WhiteTilesGameLogic();
         super.onViewCreated(view, savedInstanceState);
+
+        WhiteTilesSettings settings = new WhiteTilesSettings(getContext().getApplicationContext());
+        timerEnabled = settings.getTimerEnabled();
+        timerSetting = settings.getTimerSetting();
+
+        timer = new CountDownTimer(timerSetting * 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                String remainingTime = String.valueOf(millisUntilFinished / 1000);
+                getRemainingTimeTextView(view).setText(remainingTime);
+            }
+
+            public void onFinish() {
+                whiteButtonClicked();
+            }
+
+        };
+
+        timer.start();
 
         logic.scrambleButtons();
         colorButtons(view);
@@ -60,12 +85,23 @@ public class WhiteTilesFragment extends Fragment {
         tracker.addScore(Game.TILES, 1);
         logic.scrambleButtons();
         colorButtons(view);
+        resetTimer();
     }
 
     private void colorButtons(View view) {
         logic.getTilesButtons().forEach((buttonId, color) -> {
             view.findViewById(buttonId).setBackgroundColor(color);
         });
+    }
+
+    private TextView getRemainingTimeTextView(View view) {
+        return (TextView) view.findViewById(R.id.whiteTilesRemainingTime);
+
+    }
+
+    private void resetTimer() {
+        timer.cancel();
+        timer.start();
     }
 
 
